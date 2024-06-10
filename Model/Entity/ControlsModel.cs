9b1a3e5da60c697e7e7fbf0a48da4c25a9323087
Azhar Uddin <azhar@revusys.com>
2024-06-10 16:54:27 +0500

    Do the chnages

---
+++ Model/Entity/ControlsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Model.Entity
+{
+    public partial class ControlsModel
+    {
+        public int Id { get; set; }
+        public int? TypeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int SeqKey { get; set; }
+        public int? Parent { get; set; }
+        public bool? IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTimeOffset? CreatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTimeOffset? UpdatedDate { get; set; }
+    }
+}
